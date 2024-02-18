8e40c7b6ff2479c576223fa59d7323f68e9e7e8d
dezwemmer <56605591+dezwemmer@users.noreply.github.com>
2024-02-17 21:16:23 -0700

    added geodetic to ECEF alg

---
+++ Navigation/Algorithms/const_wgs84.py
@@ -0,0 +1,12 @@
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+# GNC Software Collection
+# Author:   Steven Anderson
+# Created:  FEB 2024
+#
+# Brief:    Constants WGS84
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+radEquator = 6378137            # equatorial radias [m]
+flatReciprocal = 298.257223563  # Reciprocal flattening (1/f) []
+angRateEarth = 7.292115e-05     # Earth angular rate [rad/s^3]
+grav = 3.986004418e14           # gravitational constant [m^3/s^2]
---
+++ Navigation/Algorithms/transform_GeodetictoECEF.py
@@ -0,0 +1,39 @@
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+# GNC Software Collection
+# Author:   Steven Anderson
+# Created:  FEB 2024
+# Credit:   Aided Navigation: GPS with High Rate Sensors,
+#           Jay A. Farrell
+# Brief:    Convert Geodetic coordinates to rectangular (ECEF)
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+import math
+import const_wgs84 as const
+
+
+def geodetic2ECEF(latRad, longRad, altMeter):
+    # define ellipsoid
+    f = 1/const.flatReciprocal
+    b = const.radEquator*(1 - f)
+    ecc = math.sqrt(f*(2 - f))
+
+    # compute normal radius (RN)
+    RN = const.radEquator / math.sqrt(1 - (ecc**2)*math.sin(latRad)**2)
+    
+    # compute ECEF coordinates
+    x = (RN + altMeter)*math.cos(latRad)*math.cos(longRad)
+    y = (RN + altMeter)*math.cos(latRad)*math.sin(longRad)
+    z = ((RN*(1 - ecc**2) + altMeter)*math.sin(latRad))
+
+    print("Latitude (rad): ",latRad)
+    print("Longitude (rad): ",longRad)
+    print("Altitude (m): ",altMeter)
+    print(">>> In ECEF >>>")
+    print(x, ",", y, ",", z)
+
+def main():
+    geodetic2ECEF(.5934119541,-2.0478571082,251.702) # rad,rad,m
+    # TODO: convert deg/min/sec to rad
+
+if __name__ == "__main__":
+    main()
